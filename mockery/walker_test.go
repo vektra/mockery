0071230b5f4ff081af62a42897bb49ea9fe4c3fe
Mike Gaffney <gaffo@justin.tv>
2016-04-06 17:47:22 -0700

    Add unit test for walker

--- mockery/walker_test.go
+++ mockery/walker_test.go
@@ -1,11 +1,49 @@
 package mockery
 
 import (
-	"testing"
-
+	"fmt"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"path"
+	"regexp"
+	"testing"
 )
 
-func TestWalk(t *testing.T) {
-	assert.Equal(t, "true", "true")
+type GatheringVisitor struct {
+	Interfaces []*Interface
+}
+
+func (this *GatheringVisitor) VisitWalk(iface *Interface) error {
+	this.Interfaces = append(this.Interfaces, iface)
+	return nil
+}
+
+func NewGatheringVisitor() *GatheringVisitor {
+	return &GatheringVisitor{
+		Interfaces: make([]*Interface, 0, 1024),
+	}
+}
+
+func TestWalkerHere(t *testing.T) {
+	wd, err := os.Getwd()
+	assert.NoError(t, err)
+	w := Walker{
+		BaseDir:   wd,
+		Recursive: true,
+		LimitOne:  false,
+		Filter:    regexp.MustCompile(".*"),
+	}
+
+	gv := NewGatheringVisitor()
+
+	w.Walk(gv)
+
+	for _, i := range gv.Interfaces {
+		fmt.Println(i)
+	}
+
+	assert.Equal(t, 17, len(gv.Interfaces))
+	first := gv.Interfaces[0]
+	assert.Equal(t, "AsyncProducer", first.Name)
+	assert.Equal(t, path.Join(wd, "fixtures/async.go"), first.Path)
 }
