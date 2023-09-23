ee33b3deeb77bd60c9407dcfbdaea6e788c01924
Rangel Reale <rangelspam@gmail.com>
2023-09-23 09:42:34 -0300

    - take type in account when using import replace types - check more specific replace types first

---
+++ pkg/fixtures/example_project/replace_type/rt.go
@@ -0,0 +1,11 @@
+package replace_type
+
+import (
+	"github.com/vektra/mockery/v2/pkg/fixtures/example_project/replace_type/rti/rt1"
+	"github.com/vektra/mockery/v2/pkg/fixtures/example_project/replace_type/rti/rt2"
+)
+
+type RType interface {
+	Replace1(f rt1.RType1)
+	Replace2(f rt2.RType2)
+}
