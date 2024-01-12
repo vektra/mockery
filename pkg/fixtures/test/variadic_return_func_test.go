d119c66e267c739ff3dc9b0d1d55e5b8090f4e9c
LandonTClipp <landonclipp@gmail.com>
2024-01-12 16:47:42 -0600

    Add test that asserts mocks on variadic return functions are behaving correctly

---
+++ pkg/fixtures/test/variadic_return_func_test.go
@@ -0,0 +1,18 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	mocks "github.com/vektra/mockery/v2/mocks/github.com/vektra/mockery/v2/pkg/fixtures"
+)
+
+func TestVariadicReturnFunc(t *testing.T) {
+	m := mocks.NewVariadicReturnFunc(t)
+	m.EXPECT().SampleMethod("").Return(func(s string, l []int, a ...any) {
+		assert.Equal(t, "foo", s)
+		assert.Equal(t, []int{1, 2, 3}, l)
+		assert.Equal(t, []any{"one", "two", "three"}, a)
+	})
+	m.SampleMethod("")("foo", []int{1, 2, 3}, "one", "two", "three")
+}
--- pkg/fixtures/variadic.go
+++ pkg/fixtures/variadic.go
@@ -6,6 +6,3 @@ type Variadic interface {
 	VariadicFunction(str string, vFunc VariadicFunction) error
 }
 
-type VariadicExtra interface {
-	SampleMethod(str string) func(str string, arr []int, a ...interface{})
-}
