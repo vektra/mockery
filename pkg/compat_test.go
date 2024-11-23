029a9f99d0454dd7e81ff93586a08970421f6356
Alysson Ribeiro <15274059+sonalys@users.noreply.github.com>
2024-11-23 08:34:06 +0100

    Fix(empty-return): move tests to correct location

--- pkg/compat_test.go
+++ pkg/compat_test.go
@@ -6,7 +6,6 @@ import (
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	mocks "github.com/vektra/mockery/v2/mocks/github.com/vektra/mockery/v2/pkg/fixtures"
-	test "github.com/vektra/mockery/v2/pkg/fixtures"
 )
 
 // CompatSuite covers compatibility with github.com/stretchr/testify/mock.
@@ -34,37 +33,6 @@ func (s *CompatSuite) TestOnAnythingOfTypeVariadicArgs() {
 	m.Sprintf("int: %d string: %s", 22, "twenty two")
 	m.AssertExpectations(s.T())
 	m.AssertCalled(t, "Sprintf", "int: %d string: %s", 22, "twenty two")
-}
-
-func (s *CompatSuite) TestOnEmptyReturn() {
-	m := mocks.NewEmptyReturn(s.T())
-	var target test.EmptyReturn = m
-
-	s.Run("NoArgs", func() {
-		run := false
-
-		m.EXPECT().NoArgs().RunAndReturn(func() {
-			run = true
-		})
-
-		target.NoArgs()
-
-		s.True(run)
-	})
-
-	s.Run("WithArgs", func() {
-		run := false
-
-		m.EXPECT().WithArgs(42, "foo").RunAndReturn(func(arg0 int, arg1 string) {
-			run = true
-			s.Equal(42, arg0)
-			s.Equal("foo", arg1)
-		})
-
-		target.WithArgs(42, "foo")
-
-		s.True(run)
-	})
 }
 
 func TestCompatSuite(t *testing.T) {
---
+++ pkg/fixtures/empty_return/interface_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test(t *testing.T) {
+	m := NewEmptyReturnMock(t)
+	var target EmptyReturn = m
+
+	t.Run("NoArgs", func(t *testing.T) {
+		run := false
+
+		m.EXPECT().NoArgs().RunAndReturn(func() {
+			run = true
+		})
+
+		target.NoArgs()
+
+		require.True(t, run)
+	})
+
+	t.Run("WithArgs", func(t *testing.T) {
+		run := false
+
+		m.EXPECT().WithArgs(42, "foo").RunAndReturn(func(arg0 int, arg1 string) {
+			run = true
+			require.Equal(t, 42, arg0)
+			require.Equal(t, "foo", arg1)
+		})
+
+		target.WithArgs(42, "foo")
+
+		require.True(t, run)
+	})
+}
