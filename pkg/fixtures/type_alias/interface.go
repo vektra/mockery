3cef7fdc3cde80a25f5791f3c6dd7fe247c01b22
LandonTClipp <11232769+LandonTClipp@users.noreply.github.com>
2024-11-26 16:43:43 -0600

    Fix bug with type aliases to structs
    @max-melentyev found in #843 that type aliases to structs were being
    rendered as a struct literal. The problem was that we need the type of the
    right hand side of the alias definition, not the underlying type itself.

--- pkg/fixtures/type_alias/interface.go
+++ pkg/fixtures/type_alias/interface.go
@@ -1,7 +1,14 @@
 package type_alias
 
+import "github.com/vektra/mockery/v2/pkg/fixtures/type_alias/subpkg"
+
 type Type = int
+type S = subpkg.S
 
 type Interface1 interface {
 	Foo() Type
+}
+
+type Interface2 interface {
+	F(Type, S, subpkg.S)
 }
--- pkg/fixtures/type_alias/interface_test.go
+++ pkg/fixtures/type_alias/interface_test.go
@@ -25,6 +25,11 @@ func TestTypeAlias(t *testing.T) {
 			filepath:      "./mock_InterfaceWithUnresolvedAlias_test.go",
 			expectedRegex: `func \((_?[a-zA-Z]*)+ \*InterfaceWithUnresolvedAlias\) Foo\(\) type_alias.Type {`,
 		},
+		{
+			name:          "Alias to type with underlying struct with resolve-type-alias: True",
+			filepath:      "./mock_Interface2WithResolvedAlias_test.go",
+			expectedRegex: `func \(_m \*Interface2WithResolvedAlias\) F\(_a0 int, _a1 subpkg.S, _a2 subpkg.S\) {`,
+		},
 	} {
 		t.Run(tt.name, func(t *testing.T) {
 			regex, err := regexp.Compile(tt.expectedRegex)
