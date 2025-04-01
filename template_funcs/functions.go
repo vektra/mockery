5b0f5df9bbadcf1880b10b32a1dcc644b1e0b168
Landon Clipp <11232769+LandonTClipp@users.noreply.github.com>
2025-03-31 23:25:05 -0500

    feat: Add a number of math functions to template FuncMap (#969)
    * feat: Add a number of math functions to template FuncMap
    
    This fixes #961.

--- template_funcs/functions.go
+++ template_funcs/functions.go
@@ -1,8 +1,12 @@
 package template_funcs
 
 import (
+	"cmp"
 	"os"
+	"slices"
 	"strings"
+
+	"golang.org/x/exp/constraints"
 )
 
 func Exported(s string) string {
@@ -17,21 +21,83 @@ func Exported(s string) string {
 	return strings.ToUpper(s[0:1]) + s[1:]
 }
 
-func ReadFile(path string) string {
+func ReadFile(path string) (string, error) {
 	if path == "" {
-		return ""
+		return "", nil
 	}
 	fileBytes, err := os.ReadFile(path)
 	if err != nil {
-		panic(err.Error())
+		return "", err
 	}
-	return string(fileBytes)
+	return string(fileBytes), nil
 }
 
-func Add(i1 int, in ...int) int {
-	var sum int = i1
+// Numbers defines the generic constraints of the arithmetic arguments.
+type Numbers interface {
+	constraints.Integer | constraints.Float | constraints.Complex
+}
+
+// Add adds the given numbers.
+func Add[T Numbers](i1 T, in ...T) T {
+	var sum T = i1
 	for _, i := range in {
 		sum += i
 	}
 	return sum
+}
+
+// Incr increments the numbers by 1.
+func Incr[T Numbers](i T) T {
+	return i + 1
+}
+
+// Decr decrements the numbers by 1.
+func Decr[T Numbers](i T) T {
+	return i - 1
+}
+
+// Sub subtracts the given numbers.
+func Sub[T Numbers](i1 T, in ...T) T {
+	var sub T = i1
+	for _, i := range in {
+		sub -= i
+	}
+	return sub
+}
+
+// Div cumulatively divides the given numbers.
+func Div[T Numbers](i1 T, in ...T) T {
+	var sub T = i1
+	for _, i := range in {
+		sub /= i
+	}
+	return sub
+}
+
+// Mod returns the cumulative modulo of the given numbers.
+func Mod[T constraints.Integer](i1 T, in ...T) T {
+	var sub T = i1
+	for _, i := range in {
+		sub = sub % i
+	}
+	return sub
+}
+
+// Mul returns the cumulative multiplication of the given numbers.
+func Mul[T Numbers](i1 T, in ...T) T {
+	var sub T = i1
+	for _, i := range in {
+		sub *= i
+	}
+	return sub
+}
+
+// Max returns the maximum value.
+func Max[T cmp.Ordered](x ...T) T {
+	return slices.Max(x)
+}
+
+// Min returns the minimum value.
+func Min[T cmp.Ordered](x ...T) T {
+	return slices.Min(x)
 }
